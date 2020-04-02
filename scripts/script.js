91b8e7143928e3bc08828a6e95ae6d7850a70f0f
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-02 22:09:40 +0300

    Начал

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-'uste strict';
+'use strict';
 
-console.log('Hello!');
+let isNumber = function(n) {
+  return !isNaN(n) && isFinite(n);
+}
+
+alert('Я загадал число от 1 до 10.\nПопробуй угадай его!');
+
+let guessNumber = function() {
+  let n = +prompt('Твой вариант: ');
+  
+}
+
+
