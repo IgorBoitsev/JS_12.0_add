1d9c8269a4c066384fa3cbe08f6b35e9c04a87d9
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-18 11:59:08 +0300

    Все до lcoalStorage

--- scripts/script.js
+++ scripts/script.js
@@ -9,17 +9,8 @@ let toDoData = [];
 
 let render = function() {
 
-  
   todoList.textContent = '';
   todoCompleted.textContent = '';
-  // toDoData = [];
-
-  for (let i = 0; i < localStorage.length; i++) {
-    
-    toDoData[i] = { value : localStorage.key(i),
-                    completed : !localStorage.getItem(localStorage.key(i))
-};
-  };
 
   toDoData.forEach(function(item){
     let li = document.createElement('li');
@@ -38,27 +29,23 @@ let render = function() {
     }
 
     // Удаление дела
-    // let btnTodoRemove = li.querySelector('.todo-remove');
-    // btnTodoRemove.addEventListener('click', function(){
-    //   // Перебераем массив и определяем индекс дела, которое необходимо удалить
-    //   for (let key in toDoData) {
-    //     if (toDoData[key].value == item.value)
-    //       localStorage.removeItem(toDoData[key].value);
-    //   };
-    //   render();
-    // });
+    let btnTodoRemove = li.querySelector('.todo-remove');
+    btnTodoRemove.addEventListener('click', function(){
+      // Перебераем массив и определяем индекс дела, которое необходимо удалить
+      for (let key in toDoData) {
+        if (toDoData[key].value == item.value)
+          toDoData.splice(key, 1);
+      };
+
+      render();
+    });
     
 
     // Добавление дел в раздел "выполенено"
     let btnTodoComplete = li.querySelector('.todo-complete');
     btnTodoComplete.addEventListener('click', function(){
-      
-      // console.log(item.completed);
-      
       item.completed = !item.completed;
-      localStorage.setItem(item.value, item.completed);
-      console.log(!!localStorage.getItem(item.value));
-      
+
       render();
     })
   })
@@ -70,7 +57,11 @@ todoControl.addEventListener('submit', function(event){
   // Проврка наличия введенных новых дел
   if (headerInput.value !== '') {
     // Добавление нового дела 
-    localStorage.setItem(headerInput.value, false);
+    let newToDo = { value : headerInput.value,
+                    complete : false
+    }
+    toDoData.push(newToDo);
+
     // Очистка поля ввода
     headerInput.value = '';
   } else {
@@ -82,14 +73,3 @@ todoControl.addEventListener('submit', function(event){
 });
 
 render();
-
-// let t = !Boolean('false');
-// // t = !t;
-// console.log(t);
-
-// let str = 'test';
-// let t = true;
-// localStorage.setItem(str, !t);
-// console.log(localStorage.key(1));
-// console.log(!!localStorage.getItem(localStorage.key(1)));
-localStorage.test = false;
